76e906c638d9c5d51eaed5a3ff84ea2516888269
CorBer <registax@gmail.com>
2022-10-14 16:42:38 +0200

    update again

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1665757884|583205411";
+const CACHE_VERSION = "1665758516|1214329528";
 const CACHE_PREFIX = "Portals-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
